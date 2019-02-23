ee128ed48589262321e22c96ddc788732eae3fbf
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-02-23 22:14:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,9 @@
 def my_collect(collection)
- # new_collection = []
+  new_collection = []
   i = 0
   while i < collection.length
     yield collection[i]
-    collection.upcase
     i += 1
- #   new_collection
   end
   collection
 end
